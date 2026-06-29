c1c5ae3e1f824d7d5d8fdf091bdeaba7a4cf41c4
Spencer Du <spencer@wearelighten.co.uk>
2026-06-29 17:01:12 +0100

    chore(api): configure OpenAPI and Swagger

--- backend/OpsFlow.Api/Program.cs
+++ backend/OpsFlow.Api/Program.cs
@@ -5,11 +5,21 @@ using OpsFlow.Infrastructure.Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "OpsFlow API v1");
+        options.RoutePrefix = "swagger";
+    });
+}
 
 using (var scope = app.Services.CreateScope())
 {
@@ -20,8 +30,17 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
 }
+
+app.MapGet("/health", () =>
+{
+    return Results.Ok(new
+    {
+        Status = "Healthy",
+        Service = "OpsFlow API",
+        Timestamp = DateTime.UtcNow
+    });
+});
 
 app.UseHttpsRedirection();
 
